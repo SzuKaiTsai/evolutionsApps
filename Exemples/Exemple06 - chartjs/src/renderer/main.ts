cd05ee224149e97f952612bbfb280473bdbf6843
1146373 <felix.charb@gmail.com>
2026-04-16 13:38:00 -0400

    start 04/16

--- Exemples/Exemple06 - chartjs/src/renderer/main.ts
+++ Exemples/Exemple06 - chartjs/src/renderer/main.ts
@@ -7,8 +7,8 @@ import router from './router';
 // Vuetify
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+import * as components from 'vuetify/lib/components';
+import * as directives from 'vuetify/lib/directives';
 import '@mdi/font/css/materialdesignicons.css';
 
 import { Participant } from "../common/participant";
